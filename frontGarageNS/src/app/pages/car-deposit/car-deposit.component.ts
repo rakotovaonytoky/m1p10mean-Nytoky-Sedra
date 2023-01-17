b0be83717f6f036198a2dee86ce70266e5e87d4b
Rakotovao <61748362+rakotovaonytoky@users.noreply.github.com>
2023-01-17 21:34:54 +0300

    mrege

--- frontGarageNS/src/app/pages/car-deposit/car-deposit.component.ts
+++ frontGarageNS/src/app/pages/car-deposit/car-deposit.component.ts
@@ -1,3 +1,4 @@
+import { Car } from './../../classes/car';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDepositComponent implements OnInit {
 
+  tableCar!: Car[];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getUnDepositCar() {
+    //   this.tableCar = [
+    //     { }
+    //   ]
   }
 
 }
--- frontGarageNS/src/app/service/login/login.service.ts
+++ frontGarageNS/src/app/service/login/login.service.ts
@@ -1,7 +1,7 @@
 import { RegisterDto } from './../../classes/register-dto';
 import { Observable } from 'rxjs/internal/Observable';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 const API_URL = environment.baseUrl;
 @Injectable({
