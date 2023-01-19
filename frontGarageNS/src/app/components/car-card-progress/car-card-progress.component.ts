d2b48731e5d49ebc59018737ef888e611041a8dd
Rakotovao <61748362+rakotovaonytoky@users.noreply.github.com>
2023-01-19 12:49:15 +0300

    car avancement page

--- frontGarageNS/src/app/components/car-card-progress/car-card-progress.component.ts
+++ frontGarageNS/src/app/components/car-card-progress/car-card-progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car-card-progress',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarCardProgressComponent implements OnInit {
 
+
+  @Input() vColor!: any;
+  @Input() vType!: any;
+  @Input() date!: any;
+  @Input() licencePlate!: any;
   constructor() { }
 
   ngOnInit(): void {
