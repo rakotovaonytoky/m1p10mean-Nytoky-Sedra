ff3287bf44fd93cffb1f1dfed556e60ed0fc9f00
Rakotovao <61748362+rakotovaonytoky@users.noreply.github.com>
2023-01-21 22:22:50 +0300

    correction affichage, sary

--- frontGarageNS/src/app/components/slider/slider.component.ts
+++ frontGarageNS/src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+  @Input() pageDescription!: any;
+  @Input() pageimage!: any;
   constructor() { }
 
   ngOnInit(): void {
+    if (!this.pageimage) {
+      this.pageimage = "/assets/img/slider-img.png";
+    }
   }
 
 }
