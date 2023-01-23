cbb1bfebb42341607c442800fad2593033549da3
promethée <sedrarandriamananjara23@gmail.com>
2023-01-23 12:54:50 +0300

    [GetConn] function databaseConnexion

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,16 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const cors = require('cors');
 const userRouter = require('./routes/users');
 const carRouter = require('./routes/car');
+const Getconn = require('./controller/connexionDb');
 //demande an'ilay moongose sinon misy error
- mongoose.set('strictQuery', true);
-
  //connexion base de donnée
-mongoose.connect('mongodb+srv://sdm:sdm@garagens.tjp2yja.mongodb.net/testtoken?authSource=admin&replicaSet=atlas-6w90rk-shard-0&readPreference=primary&ssl=true',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => { console.log('Connexion à MongoDB réussie !'); initial()})
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
 app.use(express.json());
-
-
 app.use(cors());
-
+Getconn.getConn();
 app.use('/api',userRouter);
 app.use('/api', carRouter);
-
-
 const db = require("./models/index");
 const Role = db.role;
 
