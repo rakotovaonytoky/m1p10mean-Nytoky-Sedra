54906db0b004558c13a99e650b776897ab5c4635
Rakotovao <61748362+rakotovaonytoky@users.noreply.github.com>
2023-01-27 18:02:37 +0300

    commit main

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,18 @@ const userRouter = require('./routes/users');
 const depotRouter = require('./routes/depot');
 const carRouter = require('./routes/car');
 const etatCarRouter=require('./routes/etatCar');
-
+const indexRouter = require("./routes/index");
+const path=require("path");
 //demande an'ilay moongose sinon misy error
  //connexion base de donnée
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "views")));
 Getconn.getConn();
 app.use('/api',userRouter);
 app.use('/api', carRouter);
 app.use('/api', depotRouter);
 app.use('/api', etatCarRouter);
+app.use("/**", indexRouter);
 
 module.exports = app;
